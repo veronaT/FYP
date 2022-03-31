a279873baf1da12b4f4e52fda920bb2506246a04
Verona <veronatu@hotmail.co.uk>
2022-03-31 02:02:58 +0100

    finished: fixed the 'write to files' function and added a function to generate tampered random data files

--- testData.py
+++ testData.py
@@ -1,26 +1,44 @@
 import os
+import random
 import struct
 
 """Generates the urandom code based on size specified"""
 
 
-def generate(size):
-    result = os.urandom(size)
-    print(result)
-    return result
-
-
-"""writes urandom results into new .dat file each time it's run"""
-
-
-def write(result):
-    file = "test/file.dat"
-    with open(file, 'wb') as data_file:
-        data_file.write(struct.pack('i' * len(result), *result))
+def generatePRNG(size, fileName):
+    with open('test/' + fileName + '.dat', 'wb') as f:
+        f.write(os.urandom(size))
     return "Successfully created urandom file"
 
 
+def generateFRNG(size, period, fileName):
+    sequence = []
+    key = []
+    random.seed(key)
+    c = 0
+    for i in range(0, int(size / period)):
+        for j in range(0, period):
+            temp = random.randint(0, 255)
+            if temp == key[c]:
+                while temp == key[c]:
+                    temp = random.randint(0, 255)
+            sequence.append(temp)
+    if c >= len(key) - 1:
+        c = 0
+    else:
+        c += 1
+    with open('test/' + fileName + '.dat', 'wb') as f:
+        f.write(sequence)
+    return "Successfully created tampered test sequence file"
+
+
+type = input("Specify what type of data: (1) Pseudo Random Data (2) tampered test sequence: ")
+type = int(type)
 size = input("Size of Urandom file: ")
 size = int(size)
-result = generate(size)
-print(write(result))
+fileName = input("Name of file: ")
+if type == 1:
+    print(generatePRNG(size, fileName))
+elif type == 2:
+    period = input("Enter period")
+    print(generateFRNG(size, period, fileName))
