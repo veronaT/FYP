8e52446c0c92bae92ed4d7c1e633697fa211e95d
Verona <veronatu@hotmail.co.uk>
2022-05-25 21:15:19 +0100

    started work on gui: built canvas

--- interface.py
+++ interface.py
@@ -10,12 +10,8 @@ root = tk.Tk()
 canvas = tk.Canvas(root, width=400, height=300)
 canvas.grid(columnspan=3)
 
-banner = Image.open('banner.png')
-banner = ImageTk.PhotoImage(banner)
-banner_Label = tk.Label(image=banner)
-banner_Label.image = banner
-banner_Label.grid(column=1, row=0)
 
 text = tk.Label(root, text="Select a .dat file less than 10mb")
+
 
 root.mainloop()
