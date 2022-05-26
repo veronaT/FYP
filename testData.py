b21698d3956dcba9f9a9a24c00d23f9cfd5d4498
Verona <veronatu@hotmail.co.uk>
2022-05-26 16:43:23 +0100

    found errors in the testData file and fixed the tamperData function

--- testData.py
+++ testData.py
@@ -8,27 +8,35 @@ import struct
 def generatePRNG(size, fileName):
     with open('test/' + fileName + '.dat', 'wb') as f:
         f.write(os.urandom(size))
+    f.close()
     return "Successfully created urandom file"
 
 
-def generateFRNG(size, period, fileName):
-    sequence = []
-    key = []
-    random.seed(key)
-    c = 0
-    for i in range(0, int(size / period)):
-        for j in range(0, period):
-            temp = random.randint(0, 255)
-            if temp == key[c]:
-                while temp == key[c]:
-                    temp = random.randint(0, 255)
-            sequence.append(temp)
-    if c >= len(key) - 1:
-        c = 0
-    else:
-        c += 1
-    with open('test/' + fileName + '.dat', 'wb') as f:
-        f.write(sequence)
+def generateFRNG(size, amount):
+    p = 0  # probability of the switch flipping
+    urand = 0  # random var used to determine whether switch flips in conjunction with p
+    value = 0  # a byte generated and potentially appended to the sequence
+    fname = "tamperTestData"
+
+    for a in range(0, amount):
+        filename = 'test/'+fname + str(a)
+
+        for i in range(1, amount):
+            p = i / 100
+            f = open(filename + "_" + ".bin", 'wb')
+            for j in range(0, size):
+                urand = random.uniform(0.00, 1.00)
+
+                if (urand <= p):
+                    temp = int.from_bytes(os.urandom(1), byteorder='little')
+                    while temp == 255:
+                        temp = int.from_bytes(os.urandom(1), byteorder='little')
+                    value = temp
+                else:
+                    value = int.from_bytes(os.urandom(1), byteorder='little')
+
+                f.write(bytes([value]))
+            f.close()
     return "Successfully created tampered test sequence file"
 
 
@@ -36,9 +44,11 @@ type = input("Specify what type of data: (1) Pseudo Random Data (2) tampered tes
 type = int(type)
 size = input("Size of Urandom file: ")
 size = int(size)
-fileName = input("Name of file: ")
+amount = input("Amount of files: ")
+amount = int(amount)
+
 if type == 1:
+    fileName = input("Name of file: ")
     print(generatePRNG(size, fileName))
 elif type == 2:
-    period = input("Enter period")
-    print(generateFRNG(size, period, fileName))
+    print(generateFRNG(size, amount))
